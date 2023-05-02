e680097b33c0f0dcdb35dc65930eafc1b4daa3c1
Brunonavarrooficial <bnacessoria@gmail.com>
2023-05-02 18:03:39 -0300

    new valid register in controller user
    iniciando nova validação de dados de cadastro de usuarios

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,41 @@ const User = require('../models/User')
 
 class UserController {
     static async register(req, res) {
+        const { name, email, phone, password, confirmpassword } = req.body
+
+        if (!name) {
+            res.status(422).json({ message: 'O nome é obrigatório' })
+            return
+        }
+        if (!email) {
+            res.status(422).json({ message: 'O E-mail é obrigatório' })
+            return
+        }
+        if (!phone) {
+            res.status(422).json({ message: 'O  telefone é obrigatório' })
+            return
+        }
+        if (!password) {
+            res.status(422).json({ message: 'A senha é obrigatória' })
+            return
+        }
+        if (!confirmpassword) {
+            res.status(422).json({ message: 'A confirmação da senha é obrigatório' })
+            return
+        }
+
+        if (password !== confirmpassword) {
+            res.status(422).json({ message: 'A senha e a confirmação da senha precisam ser iguais' })
+            return
+        }
+
+        const userExists = await User.findOne({ email: email })
+        if (userExists) {
+            res.status(422).json({ message: 'Por favor, ultilize outro e-mail' })
+            return
+        }
+
+
         res.json('Olá Get A Pet!')
     }
 }
