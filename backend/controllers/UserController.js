1165fdf5c582b26356c3634f8f600a61ea73cf0c
Brunonavarrooficial <bnacessoria@gmail.com>
2023-05-09 22:12:09 -0300

    create new helper middleware verify-token
    verify-token serve para verificar se o token é válido para acessar o sistema

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -117,6 +117,13 @@ class UserController {
 
         res.status(200).json({ user })
     }
+
+    static async editUser(req, res) {
+        res.status(200).json({
+            message: 'Deu certo update!'
+        })
+        return
+    }
 }
 
 module.exports = UserController
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -3,11 +3,14 @@ const router = require('express').Router()
 
 const UserController = require('../controllers/UserController')
 
+const verifyToken = require('../helpers/verify-token')
+
 //Router().post('/register', UserController.register)
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/checkuser', UserController.CheckUser)
 router.get('/:id', UserController.getUserById)
+router.patch('/edit/:id', verifyToken, UserController.editUser)
 
 //module.exports = Router
 module.exports = router
