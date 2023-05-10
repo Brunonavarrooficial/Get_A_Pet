83e1327a7b000b3ad8fc2f2db03894c870ed01d8
Brunonavarrooficial <bnacessoria@gmail.com>
2023-05-10 00:04:36 -0300

    create middleware image upload
    criação do middleware image upload para captar imagens na atualização de cadastro de usuário

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -4,13 +4,14 @@ const router = require('express').Router()
 const UserController = require('../controllers/UserController')
 
 const verifyToken = require('../helpers/verify-token')
+const { imageUpload } = require('../helpers/image-upload')
 
 //Router().post('/register', UserController.register)
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/checkuser', UserController.CheckUser)
 router.get('/:id', UserController.getUserById)
-router.patch('/edit/:id', verifyToken, UserController.editUser)
+router.patch('/edit/:id', verifyToken, imageUpload.single('image'), UserController.editUser)
 
 //module.exports = Router
 module.exports = router
