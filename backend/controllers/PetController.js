6d3762908dc9c11591f18519a245da22b6c9b5cb
Brunonavarrooficial <bnacessoria@gmail.com>
2023-05-11 20:37:07 -0300

    getall pets
    função para buscar os pets no databases e também inseriri imagens nos mesmos

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -1,9 +1,92 @@
 const Pet = require('../models/Pet')
 
+const getToken = require('../helpers/get-token')
+const getUserByToken = require('../helpers/get-user-by-token')
 class PetController {
     static async create(req, res) {
-        res.json({ message: 'Deu certo!' })
+        const { name, age, weight, color } = req.body
+
+        const images = req.files
+
+        const available = true
+
+        if (!name) {
+            res.status(422).json({ message: 'O nome é obrigatório' })
+            return
+        }
+        if (!age) {
+            res.status(422).json({ message: 'A idade é obrigatória' })
+            return
+        }
+        if (!weight) {
+            res.status(422).json({ message: 'O peso é obrigatório' })
+            return
+        }
+        if (!color) {
+            res.status(422).json({ message: 'A cor é obrigatória' })
+            return
+        }
+        if (images.length === 0) {
+            res.status(422).json({ message: 'A imagem é obrigatória' })
+            return
+        }
+
+        // get pet owner
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        const pet = new Pet({
+            name,
+            age,
+            weight,
+            color,
+            available,
+            images: [],
+            user: {
+                _id: user._id,
+                name: user.name,
+                image: user.image,
+                phone: user.phone,
+            },
+            adopter: {}
+        })
+
+        images.map((image) => {
+            pet.images.push(image.filename)
+        })
+
+        try {
+            const newPet = await pet.save()
+            res.status(201).json({
+                message: 'Pet cadastro com sucesso!',
+                newPet,
+            })
+        } catch (error) {
+            res.status(500).json({ message: error })
+        }
     }
+
+    static async getAll(req, res) {
+        const pets = await Pet.find().sort('-createdAt')
+
+        res.status(200).json({
+            pets: pets
+        })
+    }
+
+    static async getAllUserPets(req, res) {
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        const pets = await Pet.find({ 'user._id': user._id }).sort('-createdAt')
+
+        res.status(200).json({
+            pets: pets
+        })
+
+    }
+
+
 }
 
 module.exports = PetController
