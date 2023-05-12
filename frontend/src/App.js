915e51f79ab8712303e9a2dc8a96c0217622f60c
Brunonavarrooficial <bnacessoria@gmail.com>
2023-05-12 17:14:31 -0300

    create component container
    criado componente container para deixar um tamanho padrão pra pagina

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Container from './components/layout/Container';
 
 import Home from './components/pages/Home';
 import Login from './components/pages/Auth/Login';
@@ -11,11 +12,13 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/register' element={<Register />}></Route>
-        <Route path='/' element={<Home />}></Route>
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+          <Route path='/' element={<Home />}></Route>
+        </Routes>
+      </Container>
       <Footer />
     </Router>
   );
