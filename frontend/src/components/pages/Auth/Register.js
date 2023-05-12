4b71354568decec2f122e70d4d2dfdecec858965
Brunonavarrooficial <bnacessoria@gmail.com>
2023-05-12 18:47:05 -0300

    create useState for object user

--- frontend/src/components/pages/Auth/Register.js
+++ frontend/src/components/pages/Auth/Register.js
@@ -1,15 +1,25 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 import Input from "../../form/input"
 import styles from '../../form/Form.module.css'
 
 export default function Register() {
-    function handleChange(e) { }
+    const [user, setUser] = useState({})
+
+    function handleChange(e) {
+        setUser({ ...user, [e.target.name]: e.target.value })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+    }
 
     return (
         <section className={styles.form_container}>
             <h1>Registrar</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <Input text={'Nome'} type={'text'} name={'name'} placeholder={'Digite o seu nome'} handleOnChange={handleChange} />
                 <Input text={'Telefone'} type={'text'} name={'phone'} placeholder={'Digite o seu Telefone'} handleOnChange={handleChange} />
                 <Input text={'E-mail'} type={'email'} name={'email'} placeholder={'Digite o seu E-mail'} handleOnChange={handleChange} />
