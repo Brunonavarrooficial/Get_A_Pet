b485319a4953c34845ab54b12134007aee401354
Brunonavarrooficial <bnacessoria@gmail.com>
2023-05-12 17:03:56 -0300

    create routes and components navbar footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
+import Home from './components/pages/Home';
+import Login from './components/pages/Auth/Login';
+import Register from './components/pages/Auth/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
