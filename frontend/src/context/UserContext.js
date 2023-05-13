2d0eacdd1c47fa5e9623fe2a37bee77ef3f8da13
Brunonavarrooficial <bnacessoria@gmail.com>
2023-05-12 22:38:43 -0300

    auth user

--- frontend/src/context/UserContext.js
+++ frontend/src/context/UserContext.js
@@ -5,10 +5,9 @@ import useAuth from '../hooks/useAuth'
 const Context = createContext()
 
 function UserProvider({ children }) {
+    const { authenticated, register } = useAuth()
 
-    const { register } = useAuth()
-
-    return <Context.Provider value={{ register }}>{children}</Context.Provider>
+    return <Context.Provider value={{ authenticated, register }}>{children}</Context.Provider>
 
 }
 
--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,23 +1,34 @@
 import { useState, useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 import useFlashMessage from './useFlashMessage'
 
 import api from '../utils/api'
 
 export default function useAuth() {
+    const [authenticated, setAuthenticated] = useState(false)
     const { setFlashMessage } = useFlashMessage()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+
+        if (token) {
+            api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`
+            setAuthenticated(true)
+        }
+    }, [])
 
     async function register(user) {
 
-        let msgText = 'Cadastro realizado co sucesso!'
-        let msgType = 'Success'
+        let msgText = 'Cadastro realizado com sucesso!'
+        let msgType = 'success'
 
         try {
             const data = await api.post('/users/register', user)
                 .then((response) => { return response.data })
 
-            console.log(data)
+            await authUser(data)
 
         } catch (error) {
             msgText = error.response.data.message
@@ -27,5 +38,14 @@ export default function useAuth() {
         setFlashMessage(msgText, msgType)
     }
 
-    return { register }
+    async function authUser(data) {
+
+        setAuthenticated(true)
+
+        localStorage.setItem('token', JSON.stringify(data.token))
+
+        navigate('/')
+    }
+
+    return { authenticated, register }
 }
