6ccd45dfbb4b13ac04cf3071c6d6929706b282d8
Brunonavarrooficial <bnacessoria@gmail.com>
2023-05-26 16:24:03 -0300

    create new page profile and style page

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -18,6 +18,7 @@ export default function Navbar() {
             <ul>
                 <li><Link to='/'>Adotar</Link></li>
                 {authenticated ? (<>
+                    <li><Link to='/user/profile'>Perfil</Link></li>
                     <li onClick={logout}>Sair</li></>) : (<>
                         <li><Link to='/login'>Entrar</Link></li>
                         <li><Link to='/register'>Cadastrar</Link></li>
