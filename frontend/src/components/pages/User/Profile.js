2f07c96b589412f255d0a04eda4b6c97ab89197d
Brunonavarrooficial <bnacessoria@gmail.com>
2023-05-27 04:32:19 -0300

    create component imageRounded in page profile
    criação do conponente para fazer upload de imagem e também foi estilizada a página

--- frontend/src/components/pages/User/Profile.js
+++ frontend/src/components/pages/User/Profile.js
@@ -8,9 +8,11 @@ import Input from '../../form/Input'
 
 import styles from './Profile.module.css'
 import formStyles from '../../form/Form.module.css'
+import RoundedImage from '../../layout/RoundedImage'
 
 export default function Profile() {
     const [user, setUser] = useState({})
+    const [preview, setPreview] = useState()
     const [token] = useState(localStorage.getItem('token') || '')
     const { setFlashMessage } = useFlashMessage()
 
@@ -27,6 +29,7 @@ export default function Profile() {
     }, [token])
 
     function onFileChange(e) {
+        setPreview(e.target.files[0])
         setUser({ ...user, [e.target.name]: e.target.files[0] })
     }
 
@@ -64,7 +67,12 @@ export default function Profile() {
         <section>
             <div className={styles.profile_header}>
                 <h1>Perfil</h1>
-                <p>Preview Image</p>
+                {(user.image || preview) && (
+                    <RoundedImage src={
+                        preview ? URL.createObjectURL(preview) : `${process.env.REACT_APP_API}/images/users/${user.image}`}
+                        alt={user.name}
+                    />
+                )}
             </div>
             <form onSubmit={handleSubmit} className={formStyles.form_container}>
                 <Input
